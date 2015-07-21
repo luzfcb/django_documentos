d091a530b82056a4ddd0a9fa8013fecc863a748c
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2015-07-21 11:33:56 -0300

    melhorado readme.rst e templatetag

--- README.rst
+++ README.rst
@@ -3,21 +3,25 @@ django_documentos
 =============================
 
 .. image:: https://badge.fury.io/py/django_documentos.svg
-    :target: https://badge.fury.io/py/django_documentos
+     :target: https://badge.fury.io/py/django_documentos
 
 .. image:: https://travis-ci.org/luzfcb/django_documentos.svg?branch=master
-    :target: https://travis-ci.org/luzfcb/django_documentos
+     :target: https://travis-ci.org/luzfcb/django_documentos
 
-.. image:: https://img.shields.io/codecov/c/github/luzfcb/django_documentos/master.svg
-   :target: http://codecov.io/github/luzfcb/django_documentos?branch=master
+.. image:: https://requires.io/github/luzfcb/django_documentos/requirements.svg?branch=master
+     :target: https://requires.io/github/luzfcb/django_documentos/requirements/?branch=master
+     :alt: Requirements Status
+
+.. image:: http://codecov.io/github/luzfcb/django_documentos/coverage.svg?branch=master
+     :target: http://codecov.io/github/luzfcb/django_documentos?branch=master
 
 .. image:: https://landscape.io/github/luzfcb/django_documentos/master/landscape.svg?style=flat
-   :target: https://landscape.io/github/luzfcb/django_documentos/master
-   :alt: Code Health
+     :target: https://landscape.io/github/luzfcb/django_documentos/master
+     :alt: Code Health
 
 .. image:: https://badge.waffle.io/luzfcb/django_documentos.png?label=ready&title=Ready
-   :target: https://waffle.io/luzfcb/django_documentos
-   :alt: 'Stories in Ready'
+     :target: https://waffle.io/luzfcb/django_documentos
+     :alt: 'Stories in Ready'
 
 
 Simples sistema de gerenciamento de documentos digitais
@@ -36,8 +40,11 @@ Install django_documentos::
 
 Then use it in a Django Project, put on settings file, into ``INSTALLED_APPS``::
 
+    INSTALLED_APPS = [
+        ...
         'simple_history',
         'django_documentos',
+    ]
 
 and ``urls`` file::
 
--- django_documentos/templatetags/django_documentos_tags.py
+++ django_documentos/templatetags/django_documentos_tags.py
@@ -2,91 +2,10 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from django import template
-from django.forms.models import ModelFormMetaclass, ModelFormOptions, modelform_factory
-from django.utils import six
-from django.utils.encoding import force_str
+from django.forms.models import modelform_factory
+from simple_history.forms import new_readonly_form_class
 
 register = template.Library()
-
-
-class ReadOnlyFieldsMixin(object):
-    readonly_fields = ()
-
-    def __init__(self, *args, **kwargs):
-        super(ReadOnlyFieldsMixin, self).__init__(*args, **kwargs)
-        self.define_readonly_fields(self.fields)
-
-    def clean(self):
-        cleaned_data = super(ReadOnlyFieldsMixin, self).clean()
-
-        for field_name, field in six.iteritems(self.fields):
-            if self._must_be_readonly(field_name):
-                cleaned_data[field_name] = getattr(self.instance, field_name)
-
-        return cleaned_data
-
-    def define_readonly_fields(self, field_list):
-
-        fields = [field for field_name, field in six.iteritems(field_list)
-                  if self._must_be_readonly(field_name)]
-
-        map(lambda field: self._set_readonly(field), fields)
-
-    def _all_fields(self):
-        return not bool(self.readonly_fields)
-
-    def _set_readonly(self, field):
-        field.widget.attrs['disabled'] = 'true'
-        field.required = False
-
-    def _must_be_readonly(self, field_name):
-        return field_name in self.readonly_fields or self._all_fields()
-
-
-def new_readonly_form_class(klass, readonly_fields=()):
-    name = force_str("ReadOnly{}".format(klass.__name__))
-    klass_fields = {'readonly_fields': readonly_fields}
-    return type(name, (ReadOnlyFieldsMixin, klass), klass_fields)
-
-
-def is_primary_key(model_field):
-    if hasattr(model_field, 'primary_key'):
-        return True if model_field.primary_key else False
-    return False
-
-
-def new_form_class_from_model(model_instance):
-    name = force_str("{}Form".format(model_instance.__class__.__name__))
-
-    fields_instances = model_instance._meta.get_fields(include_parents=True)
-    form_fields = [(f.name, f.formfield(), f.primary_key) for f in fields_instances if
-                   hasattr(f, 'formfield') and f.primary_key is False]
-
-    fields = {key: value for (key, value, isprimary) in form_fields}
-    klass_fields = fields
-    print(klass_fields)
-    metainnerclass = type('Meta', (ModelFormOptions,), {'model': model_instance.__class__.__name__,
-                                                        'fields': [field_name for (field_name, formfield, isprimary) in
-                                                                   form_fields]})
-    print(metainnerclass)
-    klass_fields.update({
-        'Meta': metainnerclass,
-        '_meta': metainnerclass
-    })
-    try:
-
-        NewModelForm = type(name, (ModelFormMetaclass,), klass_fields)
-    except:
-        pass
-    # NewModelForm._meta = ModelFormOptions(getattr(NewModelForm, 'Meta', None))
-    return NewModelForm
-
-
-@register.filter
-def as_form2(model_instance):
-    new_form_class = new_form_class_from_model(model_instance)
-    readonly_new_form_class = new_readonly_form_class(new_form_class)
-    return readonly_new_form_class(instance=model_instance)
 
 
 @register.filter
