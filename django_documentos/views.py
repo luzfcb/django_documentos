912eed638b95f860993aa4f2896f6c70f998419a
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2015-07-22 17:09:12 -0300

    corrigido ordenacao de imports

--- django_documentos/views.py
+++ django_documentos/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
-from braces.views import LoginRequiredMixin
 
 from django.core.urlresolvers import reverse_lazy
 from django.views import generic
