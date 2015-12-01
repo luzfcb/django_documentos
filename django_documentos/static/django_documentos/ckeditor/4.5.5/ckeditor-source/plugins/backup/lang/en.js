adae16a33dc326ed3f23f192a8b6ec508bc2ba94
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2015-12-01 18:00:11 -0300

    removido compilacao ckeditor

--- django_documentos/static/django_documentos/ckeditor/4.5.5/ckeditor-source/plugins/backup/lang/en.js
+++
@@ -1,4 +0,0 @@
-﻿ CKEDITOR.plugins.setLang("backup","en",{
-  mess:'Are you sure you want to replace the existing text, the text of the backup?',
-	mess1:'Are you sure you want to remove the entire backup?',
- });
